7f952428b2669ecb2555c2026b1331f64f11807d
xichengc <xichengcdev@gmail.com>
2019-03-19 16:13:26 -0700

    change to ansible

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import utils
 
 import subprocess
 
+
 app = Flask(__name__)
 
 @app.route('/register', methods=['POST'])
 def trigger_listen():
     queue_id = json.loads(request.data)['requestBody']['queueId']
-    # return trigger_listener(queue_id)
-    return utils.make_json_response(200, queue_id)
+    return trigger_listener(queue_id)
 
 def trigger_listener(queue_id):
     subprocess.Popen(
-        ['./garage_listener.py', queue_id],
+        ['ansible-playbook', 'run.yml', '--extra-vars', '"queue_id=%s"' % queue_id],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
